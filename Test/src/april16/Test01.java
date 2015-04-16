53e7eea9109f92d640820d39054ee3da8f3fc9cb
overgd <overgd22@naver.com>
2015-04-16 14:25:18 +0900

    0416

--- Test/src/april16/Test01.java
+++ Test/src/april16/Test01.java
@@ -6,29 +6,25 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		
-		Quiz09();
-
+		Scanner sc0 = new Scanner(System.in);
+		int human = sc0.nextInt();
+		Scanner sc1 = new Scanner(System.in);
+		int com = sc1.nextInt();
+		
+		Quiz09(human, com);
 	}
 	
-	static void Quiz09(){
+	static void Quiz09(int human, int com){
 		
 		//가위 : 0, 바위 : 1, 보 : 2
 		
-		Scanner sc = new Scanner(System.in);
-		
-		int human = 1; //바위
-		int com = 0; //가위
 		int result = 0; //결과값
-		
-		human = sc.nextInt();
 		
 		result = human-com;
 		
 		if(result==1||result==-2) System.out.println("사람이 이김");
 		else if(result==0) System.out.println("비김");
 		else System.out.println("사람이 짐");
-			
-		
 	}
 
 }
---
+++ Test/src/april16/Test03.java
@@ -0,0 +1,119 @@
+package april16;
+
+import java.util.Scanner;
+
+public class Test03 {
+
+	static void methodQuiz01(int num1, int num2, int num3){
+		int cnt2=0;
+		
+		for(int cnt1 = num1;cnt1<=num2;cnt1++){
+			System.out.println(cnt1);
+			if(cnt1%num3==0){
+				cnt2++;
+				System.out.println(num3+"의 배수의 갯수 "+cnt2);
+			}
+		}
+	}
+	static void methodQuiz02(int num1, int num2, int num3){
+		int cnt2=0;
+		
+		for(int cnt1 = num1;cnt1<=num2;cnt1++){
+			//System.out.println(cnt1);
+			if(cnt1%num3==0){
+				cnt2++;
+				System.out.println(num3+"의 배수의 갯수 "+cnt2);
+			}
+		}
+		
+	}
+	static void methodQuiz03(int width, int height){
+		
+		System.out.println("사각형 도형의 면적은 "+width*height);
+		
+	}
+	static void methodQuiz04(int num1, int num2, int num3){
+		
+		int res = 0;
+		
+		if(num1>=num2){
+			res = num1;
+		}
+		else if(num1<=num2){
+			res = num2;
+		}
+		if(res<num3) res = num3;
+		
+		System.out.println("가장 큰 수는 "+res);
+		
+	}
+	static void methodQuiz05(int num1){
+		
+		System.out.println(num1+"단");
+		for(int mul=1;mul<10;mul++){
+			System.out.println(num1*mul);
+		}
+		
+	}
+	static void methodQuiz06(double inputCM){
+		
+		double outputInch = inputCM * 0.393701;
+		System.out.println(outputInch);
+		
+	}
+	static void methodQuiz07(int income){
+		
+		int won_50000 = income/50000;
+		income = income%50000;
+		int won_10000 = income/10000;
+		income = income%10000;
+		int won_5000 = income/5000;
+		income = income%5000;
+		int won_1000 = income/1000;
+		income = income%1000;
+		int won_500 = income/500;
+		income = income%500;
+		int won_100 = income/100;
+		income = income%100;
+
+		System.out.print("오만원권 : "+won_50000+" 만원권 : "+won_10000);
+		System.out.print(" 오천원권 : "+won_5000+" 천원권 : "+won_1000);
+		System.out.println(" 오백원 : "+won_500+" 백원 : "+won_100);
+	}
+	static void methodQuiz08(String ch){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String name = scan.nextLine();
+				
+		switch(ch){
+			
+		case "a": case "A":
+			System.out.println("안녕? "+name);
+			break;
+		
+		case "b": case "B":
+			System.out.println("잘가~ "+name);
+			break;
+		
+		default :
+			System.out.println("끝~ "+name);
+			break;
+		
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		methodQuiz01(1, 100, 3);
+		methodQuiz02(100, 200, 4);
+		methodQuiz03(20, 30);
+		methodQuiz04(4, 3, 5);
+		methodQuiz05(6);
+		methodQuiz06(0.1);
+		methodQuiz07(800000000);
+		methodQuiz08("a");
+	}
+
+}
