6d694346083e880050f1cf69539f43f5bb9cbe42
overgd <overgd22@naver.com>
2015-04-16 10:51:39 +0900

    0416

--- Test/src/april16/Test01.java
+++ Test/src/april16/Test01.java
@@ -1,10 +1,34 @@
 package april16;
 
+import java.util.Scanner;
+
 public class Test01 {
 
 	public static void main(String[] args) {
 		
+		Quiz09();
 
+	}
+	
+	static void Quiz09(){
+		
+		//가위 : 0, 바위 : 1, 보 : 2
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int human = 1; //바위
+		int com = 0; //가위
+		int result = 0; //결과값
+		
+		human = sc.nextInt();
+		
+		result = human-com;
+		
+		if(result==1||result==-2) System.out.println("사람이 이김");
+		else if(result==0) System.out.println("비김");
+		else System.out.println("사람이 짐");
+			
+		
 	}
 
 }
